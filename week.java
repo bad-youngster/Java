708979a1a8a3d401208ec47484cc640e9b4331a0
bad-youngster <should829@gmail.com>
2017-11-30 15:33:01 +0800

    java
    week

---
+++ week.java
@@ -0,0 +1,40 @@
+package com.test.java;
+
+/**
+ * @author wayne
+ * @version Nov 30, 2017 10:04:06 AM
+ * 
+ */
+
+public class week {
+	public static void main(String[] args) {
+		int week = 3;
+		switch (week) {
+		case 1:
+			System.out.println("today is Monday");
+			break;
+		case 2:
+			System.out.println("today is Tuesday");
+			break;
+		case 3:
+			System.out.println("today is Wednesday");
+			break;
+		case 4:
+			System.out.println("today is Thursday");
+			break;
+		case 5:
+			System.out.println("today is Friday");
+			break;
+		case 6:
+			System.out.println("today is Saturday");
+			break;
+		case 7:
+			System.out.println("today is sunday");
+			break;
+		default:
+			System.out.println("not today null");
+			break;
+		}
+	}
+
+}
